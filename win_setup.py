f3eca0cc7b63ae1bb2ded5489d9829be532cadcb
peter-dye <pjad98@gmail.com>
2020-12-20 13:14:57 -0500

    not yet working

--- win_setup.py
+++ win_setup.py
@@ -1,6 +1,15 @@
 from distutils.core import setup
 import py2exe
 
+APP = ['Calculux.py']
+DATA_FILES = ['stylesheet.qss']
+OPTIONS = {
+    'iconfile':'resources/icon.icns',
+}
+
 setup(
-    console=['Calculux.py'],
+    console=APP,
+    data_files=DATA_FILES,
+    options={'py2eze': OPTIONS},
+    setup_requires=['py2app']
 )
